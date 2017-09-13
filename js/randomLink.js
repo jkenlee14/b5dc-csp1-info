8e9bf3398e44da32b329b9a2d0b5f84a7b277aab
John Kenneth Lee <jkenlee14@gmail.com>
2017-09-13 14:24:24 +0800

    minor randomlink.js fix

--- js/randomLink.js
+++ js/randomLink.js
@@ -8,5 +8,5 @@ var randomArticle = Math.round(Math.random() * (articles.length));
 function randomLink(){
 	var randomArticle = Math.round(Math.random() *3)
 	// var randomArticle = articles[Math.floor(Math.random()*articles.length)];
-	window.location= "https://jkenlee14.github.io/b5dc-csp1-info/"+ articles[randomArticle].weburl;
+	window.location= "https://jkenlee14.github.io/b5dc-csp1-info"+ articles[randomArticle].weburl;
 }
