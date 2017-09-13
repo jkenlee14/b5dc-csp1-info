6cb561503a44ea752f073c9b2652cf8f530e16fa
John Kenneth Lee <jkenlee14@gmail.com>
2017-09-13 10:11:16 +0800

    jvector map markers work now, added carousel on the about section

--- js/draw-world.js
+++ js/draw-world.js
@@ -1,6 +1,14 @@
+var markers = [
+      {latLng: [42.79, 105.03], name: 'Gobi Desert - Mongolian Death Worm', weburl:"/deathworm.html"},
+      {latLng: [34.04, -111.09], name: 'Arizona, USA - Thunderbird', weburl:"/thunderbird.html"},
+      {latLng: [11.39, 122.63], name: 'Panay, Philippines - Aswang', weburl:"/aswang.html"},
+      {latLng: [38.84, -120.90], name: 'California - Bigfoot', weburl:"/bigfoot.html"},
+    ];
+
 $(function(){
       $('#world-map').vectorMap({
       	map: 'world_mill',
+        zoomOnScroll: false,
       	backgroundColor: '#253319',
       	markerStyle: {
     	initial: {
@@ -15,18 +23,11 @@ $(function(){
     		fill: '#c8ff0e'
   			},
     	},
-    	onMarkerClick: function(event e,index) {
-    		// $(location).attr('href', markers[index].weburl);
-    		window.location = "http://creepycryptids.x10host.com/" + markers.weburl;
-		},
-    	markers: [
-      {latLng: [42.79, 105.03], name: 'Gobi Desert - Mongolian Death Worm', weburl:"/deathworm.html"},
-      {latLng: [34.04, -111.09], name: 'Arizona, USA - Thunderbird', weburl:"/thunderbird.html"},
-      {latLng: [11.39, 122.63], name: 'Panay, Philippines - Aswang', weburl:"/aswang.html"},
-      {latLng: [38.84, 120.90], name: 'California - Bigfoot', weburl:"/bigfoot.html"},
-     
-    ]
+      markers: markers,
 
-
-      });
-    });
+    onMarkerClick: function(events, index) {
+      // console.log(markers[index].weburl);
+        window.location = "http://creepycryptids.x10host.com" +markers[index].weburl;
+    }
+  });
+});
