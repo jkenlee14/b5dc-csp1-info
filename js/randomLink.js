e44f2fcc1566564c2de12292238727af0a0176ff
John Kenneth Lee <jkenlee14@gmail.com>
2017-09-13 11:12:05 +0800

    added random link, menu links

---
+++ js/randomLink.js
@@ -0,0 +1,12 @@
+var articles = [
+	{weburl:"/deathworm.html"},
+	{weburl:"/thunderbird.html"},
+	{weburl:"/aswang.html"},
+	{weburl:"/bigfoot.html"}
+];
+var randomArticle = Math.round(Math.random() * (articles.length));
+function randomLink(){
+	var randomArticle = Math.round(Math.random() *3)
+	// var randomArticle = articles[Math.floor(Math.random()*articles.length)];
+	window.location= "http://creepycryptids.x10host.com"+ articles[randomArticle].weburl;
+}
